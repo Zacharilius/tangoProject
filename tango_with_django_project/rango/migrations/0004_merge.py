c00bac724d233f903488982f1531f54449881449
zacharilius <zabensley@gmail.com>
2015-03-06 20:41:46 -0500

    Started Django site

---
+++ tango_with_django_project/rango/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0003_category_slug'),
+        ('rango', '0002_auto_20150301_1925'),
+    ]
+
+    operations = [
+    ]
