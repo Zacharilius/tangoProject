45d18b7c34136c4b562d3b359dec7a5adffb7ead
zacharilius <zabensley@gmail.com>
2015-03-14 21:48:18 -0400

    started template for zacharilius.com

---
+++ tango_with_django_project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zacharilius/Documents/GitHub/tangoProject/tango_with_django_project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
